64b7e05741ca56fbad0860319fb74f6d9c8b1aa6
KarimPC <kardigda@gmail.com>
2021-03-29 22:47:58 -0300

    secret key

--- menu/settings.py
+++ menu/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5w4rj^)8x)i$=%m54^4zqq%y+34))pado852+zcr3g*c17cb7-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
