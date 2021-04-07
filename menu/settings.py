1b83215209e747b65f7459b35f8b79195951f178
KarimPC <kardigda@gmail.com>
2021-04-07 19:10:25 -0300

    base dir

--- menu/settings.py
+++ menu/settings.py
@@ -19,7 +19,8 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
