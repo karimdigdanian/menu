bea63c17f78ca7736bd7230dcf9ddde8b55207ad
KarimNB <kardigda@gmail.com>
2021-04-21 17:27:54 -0300

    Sengrid mail

--- menuapp/views.py
+++ menuapp/views.py
@@ -6,6 +6,11 @@ import environ # pylint: disable=import-error
 
 from .models import Plato
 
+#SendGrid imports
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 env = environ.Env()
 environ.Env.read_env()
 
@@ -28,8 +33,24 @@ def index(request):
 def contacto(request):
     return render(request, 'menuapp/contacto.html', {})
 
-
 def enviar(request):
+    message = Mail(
+    from_email='karim.lp@hotmail.com',
+    to_emails='kardigda@gmail.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+    return HttpResponseRedirect('/carta')
+
+
+def enviar2(request):
     print("def enviar")
     asunto = request.POST['asunto']
     de = request.POST['de']
