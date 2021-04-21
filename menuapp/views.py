5cce1d918b84aafe0af050e500b095bd18ae0211
KarimNB <kardigda@gmail.com>
2021-04-21 17:52:48 -0300

    formulario envio de mail

--- menuapp/views.py
+++ menuapp/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 
 import environ # pylint: disable=import-error
 
 from .models import Plato
 
-#SendGrid imports
+#SendGrid Mail imports
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient # pylint: disable=import-error
+from sendgrid.helpers.mail import Mail # pylint: disable=import-error
 
 env = environ.Env()
 environ.Env.read_env()
@@ -34,35 +33,22 @@ def contacto(request):
     return render(request, 'menuapp/contacto.html', {})
 
 def enviar(request):
+    asunto = request.POST['asunto']
+    de = request.POST['de']
+    mensaje = request.POST['mensaje']
     message = Mail(
-    from_email='karim.lp@hotmail.com',
-    to_emails='kardigda@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+        from_email = de,
+        to_emails = env('EMAIL_DESTINO'),
+        subject = asunto,
+        html_content = mensaje
+        )
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
+        
         print(response.status_code)
         print(response.body)
         print(response.headers)
     except Exception as e:
-        print(e.message)
-    return HttpResponseRedirect('/carta')
-
-
-def enviar2(request):
-    print("def enviar")
-    asunto = request.POST['asunto']
-    de = request.POST['de']
-    mensaje = request.POST['mensaje']
-    send_mail(
-        asunto,
-        mensaje,
-        de,
-        [env('EMAIL_DESTINO')],
-    )
-    print("envio")
-    
-    return HttpResponseRedirect('/carta/')
-    #return render(request, 'menuapp/index.html', {})
-    
+        print(e)
+    return HttpResponseRedirect('/index')
